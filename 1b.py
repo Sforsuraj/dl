65ef1102ca334462a418fabe746692f56658eb44
Sforsuraj <surajraj.9626@gmail.com>
2025-12-25 21:52:20 +0530

    commit

--- 1b.py
+++
@@ -1,9 +0,0 @@
-import cv2 
-import matplotlib.pyplot as plt 
- 
-img = cv2.imread(r"C:\Users\karti\Downloads\cats.jpg", cv2.IMREAD_GRAYSCALE) 
-_, thresh = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY) 
- 
-plt.subplot(1,2,1), plt.imshow(img, cmap='gray'), plt.title("Original") 
-plt.subplot(1,2,2), plt.imshow(thresh, cmap='gray'), plt.title("Thresholded") 
-plt.show()
